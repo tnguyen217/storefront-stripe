93a7c7b86216061585152dc570eb211dfcab5f2e
tnguyen217 <tnguyen217@nait.ca>
2022-04-03 02:24:55 -0600

    store front

---
+++ components/PageTitle/PageTitle.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import styles from './styles.module.scss'
+
+function PageTitle ({children, title, tagline, ...props})  {
+  return (
+        <header className={styles.pageTitle}>
+           <h1>{title || 'uiTitle' }</h1>
+           <p>{tagline || 'uiTagLine' }</p>
+
+        </header>
+  )
+}
+
+export default PageTitle
