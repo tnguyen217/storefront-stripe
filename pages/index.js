2500c7807e5e2a3ae9d62a12967fe8c4429217e2
tnguyen217 <tnguyen217@nait.ca>
2022-04-22 14:18:46 -0600

    change http-equiv to httpEquiv

--- pages/index.js
+++ pages/index.js
@@ -22,7 +22,7 @@ export default function Home(props) {
     <>
       <Head>
         <meta charset="UTF-8" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>StoreFront</title>
       </Head>
